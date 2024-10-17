e051ff8a924cdbd6b7471b84f05fecf39e847d05
OneNico <69123659+OneNico@users.noreply.github.com>
2024-10-17 08:41:01 -0300

    Add files via upload

---
+++ src/config/settings.py
@@ -0,0 +1,16 @@
+# src/config/settings.py
+
+import os
+
+# Configuraciones generales
+APP_NAME = "Visualizador de Imágenes DICOM"
+VERSION = "1.0.0"
+
+# Directorio base
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Directorio de logs
+LOG_DIR = os.path.join(BASE_DIR, 'logs')
+
+# Asegurarse de que el directorio de logs existe
+os.makedirs(LOG_DIR, exist_ok=True)
